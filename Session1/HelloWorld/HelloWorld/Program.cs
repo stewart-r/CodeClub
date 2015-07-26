ae89ead000b71cb9adee5e276ebb4e2d9168c15a
stewart_r <stewart_robertson@sky.com>
2015-07-26 14:38:52 +0100

    1

---
+++ Session1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+
+            if (name == "Subina")
+            {
+                Console.WriteLine("Bonjour " + name);
+            }
+            else
+            {
+                if (name == "Ben")
+                {
+                    Console.WriteLine("Guten tag " + name);
+                }
+                else
+                {
+                    Console.WriteLine("Hello " + name);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
