6f852f50ea1eb01a7d172d7b4a741a35aec9235c
stewart_r <stewart_robertson@sky.com>
2015-07-26 22:10:37 +0100

    1

---
+++ Session2/EncapsulationDemo/EncapsulationDemo.Step2/Solid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationDemo.Step2
+{
+    class Solid
+    {
+        public double SideLength { get; set; }
+
+        public double CalculateAreaOfFace(string shapeTypeCommand)
+        {
+
+        }
+    }
+}
