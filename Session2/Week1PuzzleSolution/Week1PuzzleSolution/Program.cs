2f790ff34ddac67d09290be5d2477015d976df24
stewartrobertson <stewartrobertson@searchlaboratory.com>
2015-07-27 17:25:03 +0100

    Added Solution for Week 1's puzzle

---
+++ Session2/Week1PuzzleSolution/Week1PuzzleSolution/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1PuzzleSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long result = 0;
+            
+            for (int i = 1; i < 1000000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
+                {
+                    result = result + i;
+                }
+            }
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
