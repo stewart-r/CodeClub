83bf040e17a886457d2998bb08364d4309a52533
stewart_r <stewart_robertson@sky.com>
2015-08-19 21:49:42 +0100

    puzzle template added

---
+++ Session3/Puzzle/Puzzle/AnagramSolver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzzle
+{
+    public class AnagramSolver
+    {
+        List<string> words;
+
+        //You can add other variables or collections here that might help....
+
+        public AnagramSolver()
+        {
+            words = File.ReadAllLines("words.txt").Select(w=>w.ToLower().Trim()).ToList();
+            //Maybe you can do some manipulation here before you begin?
+            // You can add 'class level variables' (know as fields) above
+
+        }
+
+        public IEnumerable<string> FindAnagrams(string input)
+        {
+            List<string> retVal;
+
+            //replace this with your anagram solving code//////////
+            if (input == "lemon")
+            {
+                retVal = new List<string> { "melon", "lemon" };
+            }
+            else
+            {
+                retVal = new List<string>();
+            }                                       
+            ////////////////////////////////////////////////////////
+
+            return retVal;
+        }
+    }
+}
---
+++ Session3/Puzzle/Puzzle/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzzle
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var solver = new AnagramSolver();
+            var userInput = "";
+
+            while(userInput != "X")
+            {
+                Console.WriteLine("Please enter a word to check for anagrams:");
+                userInput = Console.ReadLine();
+
+                var anagrams = solver.FindAnagrams(userInput);
+
+                if (userInput != "X")
+                {
+                    if (anagrams.Count() == 0)
+                    {
+                        Console.WriteLine("No anagrams found!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("These anagrams found:");
+                        foreach (var solution in anagrams)
+                        {
+                            Console.WriteLine(solution);
+                        }
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
